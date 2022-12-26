79f8c793dc6ee93896d17421b11e36f56ab9191b
Abel Junior <abel.costa@webjump.com.br>
2022-12-25 21:57:33 -0300

    chore: finalized refactor

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,6 @@ type Product = {
   discountRate: number;
 };
 
-// todo: [refactor] - split phases
-// this code has four responsibilities
-// first: calculate the base price
-// second: calculate discount
-// third: calculate shipping cost
-// fourth: calculate the total order price
 export function basePrice(product: Product, quantity: number): number {
   return product.basePrice * quantity;
 }
@@ -52,19 +46,3 @@ export function priceOrder(
     shippingCost(product, quantity, shippingMethod)
   );
 }
-
-let product: Product = {
-  basePrice: 1000,
-  discountThreshold: 7,
-  discountRate: 1,
-};
-
-let shipping: ShippingMethod = {
-  discountThreshold: 20,
-  discountFee: 10,
-  feePerCase: 2,
-};
-
-let orderPrice = priceOrder(product, 200, shipping);
-
-console.log(orderPrice);
